76d72d902b5abfbaf7fde955d0474d4bc36cb547
Alok Kumar Yadav <timewalker126@gmail.com>
2025-12-12 18:09:42 +0530

    Add README.md

---
+++ Day_1/Hello.java
@@ -0,0 +1,13 @@
+class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Hello Again!");
+        System.out.println("I like typing this.");
+       
+        System.out.print("Hello World");
+        //  difference between println and print is that println adds a new line after printing whereas print does not.
+        System.out.print(" This is printed\n is not on the same line.");
+        // we can also use escape sequences like \n to add a new line in print statement.
+    }
+}
+   
