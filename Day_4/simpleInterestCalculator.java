e0697bdf1effa94c158adbe2cc7aa983b25cb7c0
Vishwash <vigaur9569@gmil.com>
2026-05-10 17:10:17 +0530

    today i learned about string in java

---
+++ Day_4/simpleInterestCalculator.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class simpleInterestCalculator {
+    public static void main(String[] args) {
+       Scanner scanner = new Scanner(System.in);
+       System.out.print("Enter the principal : ");
+         double P = scanner.nextDouble();
+       System.out.print("Enter the rate of interest: ");
+         double R = scanner.nextDouble();
+       System.out.print("Enter the time period: ");
+         double T = scanner.nextDouble();
+        double SI = (P * R * T) / 100;
+        System.out.println("The simple interest is: " + SI);
+       scanner.close();
+       
+    
+
+}
+}
