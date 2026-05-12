4f16d1e2f40f287cd1813cc7963ead613dd44139
ALok Kumar Yadav <timewalker126@gmail.com>
2026-05-13 01:27:15 +0530

    today i learned control statement in java

--- Day_6/controlStatement.java
+++ Day_6/controlStatement.java
@@ -2,6 +2,10 @@
 public class controlStatement {
     public static void main(String[] args) {
         // if statement 
+        int a = 10;
+        if (a > 5) {    
+            System.out.println("a is greater than 5");
+        }       
         
         
     }
