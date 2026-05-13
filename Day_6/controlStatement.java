f2f0997cfdbc30b849740971239f408b178b93c8
ALok Kumar Yadav <timewalker126@gmail.com>
2026-05-13 23:35:16 +0530

    today i learned about control statement  in java

--- Day_6/controlStatement.java
+++ Day_6/controlStatement.java
@@ -1,11 +1,21 @@
+import java.util.Scanner;
 
 public class controlStatement {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         // if statement 
-        int a = 10;
-        if (a > 5) {    
-            System.out.println("a is greater than 5");
-        }       
+            System.out.println("Enter a number: ");
+            Double number = scanner.nextDouble();
+            if(number > 0 ){
+                System.out.println("The number is positive");
+            }
+            else if(number < 0){
+                System.out.println("The number is negative");
+            }
+            else{
+                System.out.println("The number is zero");   
+
+            }
         
         
     }
