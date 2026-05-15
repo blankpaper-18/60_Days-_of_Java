be98ce3289dd48c54d9a5456233e8243004f0f35
ALok Kumar Yadav <timewalker126@gmail.com>
2026-05-15 23:10:50 +0530

    today i learned about switch statement

---
+++ Day_7/switchStatement.java
@@ -0,0 +1,9 @@
+
+public class switchStatement {
+    public static void main(String[] args)
+    {
+        //  the switch statement offers a readable alternative to if-else-if for handling multiple conditions based on the value of a single variable.
+         
+    }
+    
+}
