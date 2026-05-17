47e1801c1679ea752a6e9aa821debad81989595a
ALok Kumar Yadav <timewalker126@gmail.com>
2026-05-17 23:03:49 +0530

    today i learned about switch statement

--- Day_7/switchStatement.java
+++ Day_7/switchStatement.java
@@ -3,6 +3,8 @@ public class switchStatement {
     public static void main(String[] args)
     {
         //  the switch statement offers a readable alternative to if-else-if for handling multiple conditions based on the value of a single variable.
+        // Enums is defined a fixed set of contants(Example days of the week)
+        // when enums are used with the switch statement:Execute differnt code blocks based on enum values,ehancing the code readability and maintaianability.
          
     }
     
